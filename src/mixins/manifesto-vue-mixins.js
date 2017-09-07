dc09cc27deffac99272fd5fd4ca2f0511e9fccd7
Shaun Ellis <shaun@sdellis.com>
2017-09-07 16:01:45 -0400

    fixes #1

--- src/mixins/manifesto-vue-mixins.js
+++ src/mixins/manifesto-vue-mixins.js
@@ -1,37 +1,35 @@
-//import manifests from './data/manifests'
-//import manifesto from '../node_modules/manifesto.js/dist/server/manifesto.js'
-//import mixins from './manifesto-react-mixins'
-//const manifestation = Object.assign(manifesto.create(JSON.stringify(manifests[0])), mixins)
+/* eslint-disable camelcase */
+import { normalize, schema } from 'normalizr'
 
 const ManifestoVueMixins = {
 
-  hello: function(){
-    console.log( "hello!" );
+  hello: function () {
+    console.log('hello!')
   },
 
-  title: function(){
-    console.log( this.getLabel() );
+  title: function () {
+    return this.getLabel()
   },
 
-  mainSequence: function(){
+  mainSequence: function () {
     // mainSequence is the one without an id (not ideal since it could have an id)
     const s = this.getSequences()
     const main_sequence = s.filter((seq) => seq.id !== 'undefined')
     return main_sequence[0]
   },
 
-  getCanvasMainThumb: function(canvas){
+  getCanvasMainThumb: function (canvas) {
     const images = canvas.getImages()
     const services = images[0].getResource().getServices()
     return services[0].id + '/full/400,/0/default.jpg'
   },
 
-  getCanvasCode: function(canvas_id){
-    const arr = canvas_id.split("/")
+  getCanvasCode: function (canvas_id) {
+    const arr = canvas_id.split('/')
     return arr.slice(-1)[0]
   },
 
-  posts: function(){
+  posts: function () {
     const s = this.mainSequence()
     const canvases = s.getCanvases()
     return canvases.map(canvas => ({
@@ -40,9 +38,45 @@ const ManifestoVueMixins = {
       likes: 0,
       id: canvas.id,
       display_src: this.getCanvasMainThumb(canvas)
-    }));
+    }))
+  },
+
+  getNormalizedTree: function () {
+    const node = new schema.Entity('nodes')
+    const nodes = new schema.Array(node)
+    node.define({ nodes })
+    const tree = new schema.Entity('tree', { nodes })
+    const toc = normalize(this.getTree(), tree)
+    window.toc = toc
+    return toc
+  },
+
+  getVueTree: function () {
+    const normalized = this.getNormalizedTree()
+    const tree = normalized.entities.tree[0].nodes
+    const nodes = normalized.entities.nodes
+
+    var toc = {
+      id: 0,
+      name: 'My Tree',
+      children: this.getChildNodes(tree, nodes)
+    }
+
+    return toc
+  },
+
+  getChildNodes: function (tree, nodes) {
+    return tree.map((id) => {
+      var node = {}
+      node.id = nodes[id].id
+      node.name = nodes[id].label
+      if (nodes[id].nodes.length) {
+        node.children = this.getChildNodes(nodes[id].nodes, nodes)
+      }
+      return node
+    })
   }
 
 }
 
-export default ManifestoVueMixins;
+export default ManifestoVueMixins
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Tree from '@/components/Tree'
-import manifestation from '../data/sample'
+import manifests from '../data/manifests'
+import manifesto from '../../node_modules/manifesto.js/dist/server/manifesto.js'
+import mixins from '../mixins/manifesto-vue-mixins'
+const manifestation = Object.assign(manifesto.create(JSON.stringify(manifests[0])), mixins)
 
+window.manifestation = manifestation
 Vue.use(Router)
 
 export default new Router({
@@ -11,7 +15,7 @@ export default new Router({
       path: '/',
       name: 'Tree',
       component: Tree,
-      props: { manifestation: manifestation }
+      props: { toc: manifestation.getVueTree() }
     }
   ]
 })
